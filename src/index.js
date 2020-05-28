118f4466418810f6e8fbf731d623b7a3dce67a3c
github username <github email address>
2020-05-28 10:31:42 -0400

    realized i was on advanced by mistake

--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const posterDiv = () => document.querySelector("#poster")
     const showingDiv = () => document.querySelector("#showing")
 
+    //oops...started advanced deliverables by mistake
+    const filmItems = document.getElementsByClassName('film item') 
+    const filmItmArray = Array.from(filmItems)
+    const movieList = filmItmArray[1]
+
+  //fetch all movies and render them in div w/ class card below 2nd div film item
+  const getMovies = () => {
+      fetch(url)
+      .then(resp => resp.json())
+      .then(renderAllMovies)
+  }  
+
+  //render each movie in div class card below film item
+  //grab 2nd div film item
+  const renderAllMovies = movies => {
+      movies.forEach(movie => {
+        
+      })
+  }
+
   //fetch a single movie and render its 
   //poster, title, runtime, showtime, and available tickets
   const getSingleMovie = movie => {
@@ -23,18 +43,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const showtime = document.querySelector('#showtime')
     showtime.textContent = movie.showtime 
 
+    //get available tx by subtracting txsold from capacity
     const capacity = movie.capacity
     const txSold = movie.tickets_sold
-    const availTx = capacity - txSold
+    const sum = capacity - txSold
     
     const availableTx = document.querySelector('#ticket-num')
-    availableTx.textContent = placeholder
+    availableTx.textContent = sum 
 
     //deliverables doesn't ask for description, but I added just in case
     const desc = document.querySelector('#film-info')
     desc.textContent = movie.description
   }
 
+  getMovies()
   getSingleMovie()
 })
 
