69b2d2badd53b72f76cf9fc410b78bb2d69bca0a
Section9-Togusa <corbindac@gmail.com>
2020-05-28 10:11:46 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,3 +2,23 @@ const url = "http://localhost:3000/films"
 const filmsDiv = () => document.querySelector("#films")
 const posterDiv = () => document.querySelector("#poster")
 const showingDiv = () => document.querySelector("#showing")
+
+url = "http://localhost:3000/films"
+
+document.addEventListener('DOMContentLoaded', (events) => {
+    fetch(url)
+    .then(res => res.json())
+    .then(films => {
+        const filmList = document.querySelector('#films')
+        films.forEach(f => insertFilm(f, filmList))
+        filmList.addEventListener('change', event => {
+            const showingDiv = document.querySelector('#showing')
+            displayFilm(event.target.value, showingDiv)
+        })
+    })
+})
+
+document.addEventListener("click", (event) => {
+    if (event.target.)
+
+})
