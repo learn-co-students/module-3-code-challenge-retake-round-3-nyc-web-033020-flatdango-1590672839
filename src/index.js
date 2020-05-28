96c73ee9245237ae1c1694057fbf0aa13e1a059c
github username <github email address>
2020-05-28 10:31:20 -0400

    2nd Commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,32 @@ const filmsDiv = () => document.querySelector("#films")
 const posterDiv = () => document.querySelector("#poster")
 const showingDiv = () => document.querySelector("#showing") 
 
+function getFilms() {
+    fetch(url) 
+    .then(resp => resp.json()) 
+    .then(json => renderFilms(div))
+}
+
 function getFilm(id) {
-    fetch(`${url}/1`) 
+    fetch(url+id)  
         .then(res => res.json()) 
         .then(json => displayFilmInfo(json))  
 } 
 
+function remainingTickets(id, capacity, tickets_sold) {
+    fetch(url + id, {
+        method: "PATCH", 
+        headers: {
+            "Content-Type": "application/json", 
+            Accept: "application/json" 
+        },
+    })
+}
+
 function displayFilmInfo(film) {
+    posterDiv.dataset.id = film.id 
     posterDiv.innerHTML = `<img id="poster" src= ${film.image}>` 
+    showingDiv.dataset.id = film.id 
     let remainingTickets = film.capacity - film.tickets_sold 
     console.log(remainingTickets)
     showingDiv.innerHTML = `<div class="card">
@@ -18,13 +36,13 @@ function displayFilmInfo(film) {
     <div id="runtime" class="meta">${film.runtime} minutes</div>
     <div class="content">
         <div class="description">
-            <div id="film-info">[INSERT MOVIE DESCRIPTION HERE]</div>
-            <span id="showtime" class="ui label">[SHOWTIME]</span>
-            <span id="ticket-num">[X]</span> remaining tickets
+            <div id="film-info">${film.description}</div>
+            <span id="showtime" class="ui label">${film.showtime}</span>
+            <span id="ticket-num">${film.remainingTickets}</span> remaining tickets
         </div>
     </div>
     <div class="extra content">
       <div class="ui orange button">Buy Ticket</div>
     </div>
-  </div>`
+  </div>` 
 }
