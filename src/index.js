1d13ec16eaff953b99e9db02c70d8612cc6865b8
github username <github email address>
2020-05-28 10:11:05 -0400

    first

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ const url = "http://localhost:3000/films"
 const filmsDiv = () => document.querySelector("#films")
 const posterDiv = () => document.querySelector("#poster")
 const showingDiv = () => document.querySelector("#showing")
+
+fetch(url)
+.then(res => res.json())
+.then(data => {
+    let filmOne = data[0]
+    let posterImg = document.getElementById('poster').src(filmOne.poster)
+    posterDiv.src(filmOne.poster)  
+})
