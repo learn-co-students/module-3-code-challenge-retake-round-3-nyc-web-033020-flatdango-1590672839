13023eceb876cdcde602354396feb7db47fc0c3c
github username <github email address>
2020-05-28 11:11:27 -0400

    4th commit

--- src/index.js
+++ src/index.js
@@ -7,32 +7,30 @@ const remainingTickets = film.capacity - film.tickets_sold
 function getFilms() {
     fetch(url) 
     .then(resp => resp.json()) 
-    .then(json => renderFilms(div))
+    .then(films => renderFilms(films))
 }
-
-function getFilm(id) {
-    fetch(url+id)  
-        .then(res => res.json()) 
-        .then(json => displayFilmInfo(json))  
-} 
-
-function remainingTickets(id, remainingTickets) {
-    fetch(url + id, {
-        method: "PATCH", 
-        headers: {
-            "Content-Type": "application/json", 
-            Accept: "application/json" 
-        },
-        body: JSON.stringify({
-            remainingTickets: remainingTickets
-        })
-    })
-} 
 
 function renderFilms(films) {
-    let li = document.createElement("li") 
+    const list = document.getElementById("film-list") 
+    filmsDiv.appendChild(list)  
+    films.forEach( film => {
+        const li = document.createElement("li") 
+        li.className = "film-group-item" 
+        li.innerText = film.title 
+        li.id = film.id 
+        list.appendChild(li) 
+    })
 }
 
+function showFilm() {
+    const list = document.getElementById("film-list") 
+    list.addEventListener("click", e => {
+        const film_id = e.target.id 
+        fetch(`${url}/${film_id}`) 
+            .then(resp => resp.json()) 
+            .then(film => displayFilmInfo(film)) 
+    })
+}
 function displayFilmInfo(film) {
     posterDiv.dataset.id = film.id 
     posterDiv.innerHTML = `<img id="poster" src= ${film.image}>` 
