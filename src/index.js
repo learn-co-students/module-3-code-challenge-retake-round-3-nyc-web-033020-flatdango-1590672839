b0bbf6881691f9a70a422ed608e72adfeebc66a6
RBRNFRVR <rommelchavez99@gmail.com>
2020-05-28 10:11:36 -0400

    first deliverable

--- src/index.js
+++ src/index.js
@@ -2,3 +2,31 @@ const url = "http://localhost:3000/films"
 const filmsDiv = () => document.querySelector("#films")
 const posterDiv = () => document.querySelector("#poster")
 const showingDiv = () => document.querySelector("#showing")
+document.addEventListener("DOMContentLoaded", event => {
+    getMovieInfo()
+})
+
+function getMovieInfo(){
+    let firstMovie = 1
+ fetch(`${url}/${firstMovie}`)
+.then(response => response.json())
+.then(data => displayMovieInfo(data))
+}
+
+function displayMovieInfo(data){
+    let poster = document.querySelector('#poster')
+    poster.src = `${data.poster}`
+    let title = document.querySelector('#title')
+    title.innerHTML = `${data.title}`
+    let runtime = document.querySelector(`#runtime`)
+    runtime.innerHTML = `${data.runtime} mins`
+    let description = document.querySelector('#film-info')
+    description.innerHTML = `${data.description}`
+    let showtime = document.querySelector('#showtime')
+    showtime.innerHTML = `${data.showtime}`
+    let tickets = document.querySelector('#ticket-num')
+    let ticketsSold = data.tickets_sold
+    let capacity = data.capacity
+    let remainingTickets = capacity - ticketsSold
+    tickets.innerHTML = remainingTickets
+}
