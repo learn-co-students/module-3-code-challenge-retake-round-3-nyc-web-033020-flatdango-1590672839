2dade9bf9552e7d332f2cd6350a0c7f39758938f
github username <github email address>
2020-05-28 11:11:23 -0400

    fourth

--- src/index.js
+++ src/index.js
@@ -1,36 +1,53 @@
-const url = "http://localhost:3000/films/1"
-const filmsDiv = () => document.querySelector("#films")
-const posterDiv = () => document.querySelector("#poster")
-const showingDiv = () => document.querySelector("#showing")
+document.addEventListener('DOMContentLoaded', function() {
 
-fetch(url)
-.then(res => res.json())
-.then(data => {
-    let filmOne = data
-    document.getElementById('poster').src = filmOne.poster
-    document.getElementById('title').innerText = filmOne.title 
-    document.getElementById('runtime').innerText = filmOne.runtime + ' minutes'
-    document.getElementById('showtime').innerText = filmOne.showtime 
-    document.getElementById('film-info').innerText = filmOne.description 
-    document.getElementById('ticket-num').innerText = parseInt(filmOne.capacity) - filmOne.tickets_sold
+    const url = "http://localhost:3000/films/1"
+    const filmsDiv = () => document.querySelector("#films")
+    const posterDiv = () => document.querySelector("#poster")
+    const showingDiv = () => document.querySelector("#showing")
 
-    const buyBtn = document.getElementById('buy-btn')
-    buyBtn.addEventListener('click', event => {
-        if(filmOne.capacity != filmOne.tickets_sold) {
-            filmOne.tickets_sold += 1 
-            fetch(url, {
-                method: 'PATCH',
-                headers: {
-                    'accept': 'application/json',
-                    'content-type': 'application/json; charset=UTF-8'
-                }
-                // body: JSON.stringify(filmOne)
-            })
-            .then(response => response.json())
-            .then(data => console.log(data))
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        let filmOne = data
+        document.getElementById('poster').src = filmOne.poster
+        document.getElementById('title').innerText = filmOne.title 
+        document.getElementById('runtime').innerText = filmOne.runtime + ' minutes'
+        document.getElementById('showtime').innerText = filmOne.showtime 
+        document.getElementById('film-info').innerText = filmOne.description 
+        document.getElementById('ticket-num').innerText = parseInt(filmOne.capacity) - filmOne.tickets_sold
+
+        const buyBtn = document.getElementById('buy-btn')
+        if(filmOne.capacity == filmOne.tickets_sold){
+            buyBtn.innerText = 'SOLD OUT'
         }
-
+        buyBtn.addEventListener('click', event => {
+            if(filmOne.capacity != filmOne.tickets_sold) {
+                filmOne.tickets_sold += 1 
+                fetch(url, {
+                    method: 'PATCH',
+                    headers: {
+                        'accept': 'application/json',
+                        'content-type': 'application/json; charset=UTF-8'
+                    },
+                    body: JSON.stringify(filmOne)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById('ticket-num').innerText = parseInt(filmOne.capacity) - filmOne.tickets_sold
+                    if(filmOne.capacity == filmOne.tickets_sold){
+                        buyBtn.innerText = 'SOLD OUT'
+                    }
+                })
+            }
+        })
     })
 
+    ////
 
+    const allFilms = films => {
+        let filmDiv = document.querySelector('film item')
+        films.forEach(film => console.log(film))
+        
+        // filmDiv.innerText = 
+    }
 })
