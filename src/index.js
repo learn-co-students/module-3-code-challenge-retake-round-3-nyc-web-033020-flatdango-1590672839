493a60a7325155e84b5cdeca43c11c139274a785
github username <github email address>
2020-05-28 10:31:02 -0400

    second

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000/films"
+const url = "http://localhost:3000/films/1"
 const filmsDiv = () => document.querySelector("#films")
 const posterDiv = () => document.querySelector("#poster")
 const showingDiv = () => document.querySelector("#showing")
@@ -6,7 +6,21 @@ const showingDiv = () => document.querySelector("#showing")
 fetch(url)
 .then(res => res.json())
 .then(data => {
-    let filmOne = data[0]
-    let posterImg = document.getElementById('poster').src(filmOne.poster)
-    posterDiv.src(filmOne.poster)  
+    let filmOne = data
+    document.getElementById('poster').src = filmOne.poster
+    document.getElementById('title').innerText = filmOne.title 
+    document.getElementById('runtime').innerText = filmOne.runtime + ' minutes'
+    document.getElementById('showtime').innerText = filmOne.showtime 
+    document.getElementById('film-info').innerText = filmOne.description 
+    document.getElementById('ticket-num').innerText = parseInt(filmOne.capacity) - filmOne.tickets_sold
+
+    const buyBtn = document.getElementById('buy-btn')
+
+    buyBtn.addEventListener('click', event => {
+        if(filmOne.capacity != filmOne.ticket_sold) {
+            fetch()
+        }
+    })
+
+
 })
